e82fc611408370a93422ff26c8ca924c3f2e998d
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-08 17:58:06 -0400

    added dynamic elements to second javaScript file

--- assets/JS/second.js
+++ assets/JS/second.js
@@ -1,3 +1,4 @@
+var resultsEL = document.getElementById('results');
 
 //https://www.loc.gov/'
 
@@ -9,6 +10,8 @@ function getApi(){
         var splitQuery = query.split('&')
         for(var i = 0; i < splitQuery.length ; i++){
     
+
+
             info.push(splitQuery[i].split("=")[1]);
             console.log(info)
         }
@@ -21,7 +24,33 @@ function getApi(){
         }
     })
     .then(function(data){
+         for (var i = 0; i < 10 ; i++) {
+            var inputTitle = document.createElement("h3");
+            var inputDescription = document.createElement('p');
+            var inputDate = document.createElement("p")
+            var containerDiv = document.createElement("div");
+
+            containerDiv.setAttribute("class", "row bg-warning m-3");
+
+            
+            inputTitle.textContent = data.results[i].title;
+            inputDate.textContent = data.results[i].date;
+            inputDescription.textContent = data.results[i].description;
+            
+            console.log(inputDescription);
+            
+            containerDiv.appendChild(inputTitle);
+            containerDiv.appendChild(inputDate);
+            containerDiv.appendChild(inputDescription);
+
+            resultsEL.appendChild(containerDiv)
+   
+         };
+         
+        
+
         console.log(data);
+
         
     });
 
