a9276f2eb5460d7f490c1e509b31357d134012db
michael seaman <mseaman26@gmail.com>
2022-10-08 13:43:58 -0700

    we are able to get search results now

--- assets/JS/second.js
+++ assets/JS/second.js
@@ -2,18 +2,19 @@
 //https://www.loc.gov/'
 
 let query = document.location.search;
-var info = [];      //for input and format
-var LibOfCongressLink = 'https://www.loc.gov/' /* + info[]*/;
-
-var splitQuery = query.split('&')
-for(var i = 0; i < splitQuery.length ; i++){
-    info.push(splitQuery[i].split("=")[1]);
-    console.log(info)
-}
 
 
 function getApi(){
-    fetch('https://www.loc.gov/maps/?q=civil war&fo=json')
+    var info = []
+        var splitQuery = query.split('&')
+        for(var i = 0; i < splitQuery.length ; i++){
+    
+            info.push(splitQuery[i].split("=")[1]);
+            console.log(info)
+        }
+        var LibOfCongressLink = 'https://www.loc.gov/'+info[1]+"/?q="+info[0]+"&fo=json"
+        console.log(LibOfCongressLink)
+    fetch(LibOfCongressLink)
     .then( function(response){
         if(response.ok){
             return response.json();
@@ -21,7 +22,9 @@ function getApi(){
     })
     .then(function(data){
         console.log(data);
+        
     });
+
 }
 
 getApi();
